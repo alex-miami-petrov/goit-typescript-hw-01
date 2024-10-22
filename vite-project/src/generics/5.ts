c88445d5fbde98d4640973b8606528aeb825612b
Miami <alex.miami.petrov@gmail.com>
2024-10-22 12:05:09 +0300

    m

---
+++ vite-project/src/generics/5.ts
@@ -0,0 +1,12 @@
+import { UserRole } from "./5";
+export enum UserRole {
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
+}
+
+const RoleDescription: Record<UserRole, string> = {
+  [UserRole.admin]: "Admin User",
+  [UserRole.editor]: "Editor User",
+  [UserRole.guest]: "Guest User",
+};
